91976fd69cba81036ed087250ba6f7d835b31d79
Simon Ho <simonho.ubc@gmail.com>
2023-04-23 02:06:33 -0700

    Create class to handle configuration

--- src/informant/informant.py
+++ src/informant/informant.py
@@ -1,44 +1,52 @@
 import traceback
 from functools import wraps
-from typing import Union
+from typing import Any, Callable, Union
 
 from informant.messengers import Messenger
 
-
 # NOTE: what should the API look like? message strings to decorator or to messengers?
-def informant(messengers: Union[Messenger, list[Messenger]], send_result: bool = False):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            try:
-                header = f"Task '{func.__name__}' has finished"
-                result = func(*args, **kwargs)
-                has_errored = False
-            except Exception as e:
-                header = e
-                result = traceback.format_exc()
-                has_errored = True
+# NOTE: should 'messengers' actually be 'targets'?
 
-            opts = {"function": func.__name__, "has_errored": has_errored}
 
-            if send_result:
-                opts["header"] = str(header)
-                # HACK: match for class type desktopmessenger.
-                # message needs to be <256 chars, but ONLY for desktop
-                opts["message"] = str(result)[:256]
-            else:
-                status = "successfully." if not has_errored else "with errors."
-                opts["header"] = "Task Status"
-                opts["message"] = f"Task '{func.__name__}' has finished {status}"
+class Informant:
+    def __init__(self, config=None):
+        self.config = config
 
-            if isinstance(messengers, list):
-                for messenger in messengers:
-                    messenger.notify(**opts)
-            else:
-                messengers.notify(**opts)
+    def __call__(
+        self, messengers: Union[Messenger, list[Messenger]], send_result: bool = False
+    ) -> Callable:
+        def decorator(func: Callable) -> Callable:
+            @wraps(func)
+            def wrapper(*args: Any, **kwargs: Any):
+                try:
+                    header = f"Task '{func.__name__}' has finished"
+                    result = func(*args, **kwargs)
+                    has_errored = False
+                except Exception as e:
+                    header = e
+                    result = traceback.format_exc()
+                    has_errored = True
 
-            return result
+                opts = {"function": func.__name__, "has_errored": has_errored}
 
-        return wrapper
+                if send_result:
+                    opts["header"] = str(header)
+                    # HACK: match for class type desktopmessenger.
+                    # message needs to be <256 chars, but ONLY for desktop
+                    opts["message"] = str(result)[:256]
+                else:
+                    status = "successfully." if not has_errored else "with errors."
+                    opts["header"] = "Task Status"
+                    opts["message"] = f"Task '{func.__name__}' has finished {status}"
 
-    return decorator
+                if isinstance(messengers, list):
+                    for messenger in messengers:
+                        messenger.notify(**opts)
+                else:
+                    messengers.notify(**opts)
+
+                return result
+
+            return wrapper
+
+        return decorator
