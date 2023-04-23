dc639042789c7d4f94e317ff4d814b0f55e9cc3a
Simon Ho <simonho.ubc@gmail.com>
2023-04-23 00:54:26 -0700

    Pass along information about completion status

--- src/informant/informant.py
+++ src/informant/informant.py
@@ -1,22 +1,41 @@
+import traceback
 from functools import wraps
 from typing import Union
 
 from informant.messengers import Messenger
 
 
-def informant(messenger: Union[Messenger, list[Messenger]]):
+# NOTE: what should the API look like? message strings to decorator or to messengers?
+def informant(messengers: Union[Messenger, list[Messenger]], send_result: bool = False):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            result = func(*args, **kwargs)
+            try:
+                header = f"Task '{func.__name__}' has finished"
+                result = func(*args, **kwargs)
+                has_errored = False
+            except Exception as e:
+                header = e
+                result = traceback.format_exc()
+                has_errored = True
 
-            if isinstance(messenger, list):
-                print(f"Sending notification to {len(messenger)} messengers")
-                for method in messenger:
-                    method.notify()
+            opts = {"function": func.__name__, "has_errored": has_errored}
+
+            if send_result:
+                opts["header"] = str(header)
+                # HACK: match for class type desktopmessenger.
+                # message needs to be <256 chars, but ONLY for desktop
+                opts["message"] = str(result)[:256]
             else:
-                print(f"Sending notification to {messenger}")
-                messenger.notify()
+                status = "successfully." if not has_errored else "with errors."
+                opts["header"] = "Task Status"
+                opts["message"] = f"Task '{func.__name__}' has finished {status}"
+
+            if isinstance(messengers, list):
+                for messenger in messengers:
+                    messenger.notify(**opts)
+            else:
+                messengers.notify(**opts)
 
             return result
 
--- src/informant/messengers/messenger.py
+++ src/informant/messengers/messenger.py
@@ -2,6 +2,8 @@ from abc import ABC, abstractmethod
 
 
 class Messenger(ABC):
+    # HACK: **kwargs feels a bit dirty as it becomes hard to type check
+    # How to do this since different messengers require different opts?
     @abstractmethod
-    def notify(self) -> None:
+    def notify(self, **kwargs) -> None:
         pass
